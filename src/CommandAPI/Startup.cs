bcfe7f1ebb49e3fc23dc38e53f137966ade621c2
Jim Marten <JimMarten@gmail.com>
2022-05-20 14:16:01 -0500

    Added DTO and Functionality to use it

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
 
 namespace CommandAPI
 {
@@ -35,6 +36,7 @@ namespace CommandAPI
 
             services.AddControllers();
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
